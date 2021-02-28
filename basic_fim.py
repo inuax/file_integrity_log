ce0fd7fac76f0004119961dbd08c921472ac3c28
Vatcharin Kongsakul <ghinryudokung@gmail.com>
2021-02-28 09:53:52 +0700

    initialcommit

---
+++ basic_fim.py
@@ -0,0 +1,72 @@
+import os, hashlib, time, datetime
+
+
+# improvise log function
+def fim_log(msg):
+    f = open('fimlog/fimlog.txt', 'a')
+    f.write(msg + '\n')
+    f.close()
+
+
+files = {}
+while True:
+    for file in os.listdir('.'):
+        if os.path.isfile(file):
+            hash = hashlib.md5()
+            with open(file, 'rb') as f:
+                for chunk in iter(lambda: f.read(4096), b""):
+                    hash.update(chunk)
+                md5 = hash.hexdigest()
+            if file in files and md5 != files[file]:
+                msg = file + ' has been change at ' + str(datetime.datetime.now())
+                print(msg)
+                fim_log(msg)
+
+            files[file] = md5
+    time.sleep(1)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# files = {}
+# while True:
+#     for file in os.listdir('.'):
+#         if os.path.isfile(file):
+#             hash = hashlib.md5()
+#             with open(file, "rb") as f:
+#                 for chunk in iter(lambda: f.read(4096), b""):
+#                     hash.update(chunk)
+#             md5 = hash.hexdigest()
+#             if file in files and md5 != files[file]:
+#                 print('%s\t%s has been changed!'%(time.strftime("%Y-%m-%d %H:%M:%S"), file))
+#             files[file] = md5
+#             print(files)
+#     time.sleep(1)
